17d1ad9442aade6ff14bd0b23b5b537f161c16dd
Arganata <arganata.on@gmail.com>
2025-02-06 20:02:08 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+echo "I like pizza <br>";
+echo "It's really good";
+// This is an comment
+/*
+This
+is
+a
+multiline
+comment
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+
+</html>
