f6ddb6ad24ca46d59c0961d8f3a55c92c102ceb3
Arganata <arganata.on@gmail.com>
2025-08-26 08:33:56 +0700

    Add logical operators reference in PHP
    Created logical_operators.php with comments explaining the use of logical operators (&&, ||, !) in PHP for combining conditional statements.

---
+++ logical_operators.php
@@ -0,0 +1,8 @@
+<?php
+// Logical operators = combine conditional statements
+// if (condition && condition2)
+
+// && = True if both conditions are true
+// || = True if at least one condition is true
+// ! = True if false. False if true.
+
